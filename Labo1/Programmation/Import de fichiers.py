c4ab95e46b60fc9672c122ae9059ea069fde0552
Clement-Tremblay <CLTRE295@ulaval.ca>
2025-09-09 20:53:37 -0400

    Détection nombre colonnes

--- Labo1/Programmation/Import de fichiers.py
+++ Labo1/Programmation/Import de fichiers.py
@@ -2,23 +2,31 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np    
 
-#Labo1/Mesures/tension_patate_aluacier_90925_01.lvm
-#Labo1/Mesures/tension_patate_aluacier_90925_02.lvm
-#Labo1/Mesures/tension_patate_aluacier_90925_03.lvm
-#Labo1/Mesures/tension_patate_aluinox_090925_01.lvm
-#Labo1/Mesures/tension_patate_aluinox_090925_02.lvm
-#Labo1/Mesures/voltage_circuit_090925_01.lvm
-#Labo1/Mesures/voltage_pile_090925_01.lvm
 
+# Les fichiers de données dans l'ordres demandés par le protocoles. Cela permet de simplement appeler files[4] par ex.
+files = ['Labo1/Mesures/convertisseur_090925_01.lvm',
+         'Labo1/Mesures/convertisseur_débranché_090925_01.lvm',
+         'Labo1/Mesures/convertisseur_débranché_090925_02.lvm',
+         'Labo1/Mesures/tension_patate_aluinox_090925_01.lvm',
+         'Labo1/Mesures/tension_patate_aluinox_090925_02.lvm',
+         'Labo1/Mesures/tension_patate_aluacier_90925_01.lvm',
+         'Labo1/Mesures/tension_patate_aluacier_90925_02.lvm',
+         'Labo1/Mesures/tension_patate_aluacier_90925_03.lvm',
+         'Labo1/Mesures/voltage_pile_090925_01.lvm',
+         'Labo1/Mesures/voltage_circuit_090925_01.lvm']
 
 filepath = "Labo1/Mesures/convertisseur_débranché_090925_01.lvm"
 
 
 def read(file_name):
     df = pd.read_csv(file_name, sep="\t", skiprows=22, decimal=",")
-    df = df.iloc[:, 1:].copy() 
+    df = df.iloc[:, 1:].copy()
 
-    return df.to_numpy()[:, :2]
+    col = 1 # On ne prend qu'une seule colonne si nous n'avons qu'une seule colonne de données.
+    if df.shape[1] == 3:    # On prend 2 colonnes si on a deux colonnes de données (3 colonnes données par pandas).
+        col = 2
+
+    return df.to_numpy()[:, :col]
 
 print(read(filepath))
 
