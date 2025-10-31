47b0d52770bdf868cbe111ac35fa32dbcc5014a1
Clement-Tremblay <CLTRE295@ulaval.ca>
2025-10-31 15:57:25 -0400

    Calcul puissance moyenne dissipée

--- Labo5/Programmation/Partie 1.py
+++ Labo5/Programmation/Partie 1.py
@@ -4,7 +4,7 @@ import os
 
 
 # Dossier où se trouvent les fichiers
-filesfolder = os.path.join("Labo5/Mesures/Partie1/")
+folder = os.path.join("Labo5/Mesures/Partie1/")
 
 def read(file_name):
     df = pd.read_csv(file_name, sep="\t", skiprows=21, decimal=",")
@@ -12,4 +12,22 @@ def read(file_name):
 
     return df.to_numpy()[:, :2]
 
+def puissance_moyenne_dissipee(arr):
+    """
+    Prend un array avec la tension DC RMS et la résistance et retourne un array de même taille avec p_moy dissipée
 
+    Paramètres
+    arr : array numpy
+        La colonne 1 contient la résistance et la colonne deux contient la tension DC RMS
+    
+    Retourne
+    p_moy_dis : array
+        Array des puissances dissipées
+    """
+    
+    r = arr[:, 0]   # Colonne de résistance
+    v = arr[:, 1]   # Colonne de tension
+
+    p_moy_dis = v**2 / r    # Array de la puissance moyenne dissipée
+
+    return p_moy_dis
